d876c87d29c644e12e4a342906f8c2c5a1df5854
hara29 <cindymaharani29@gmail.com>
2024-06-14 21:38:42 +0700

    menambahkan description dan tips

---
+++ app/src/main/java/com/dicoding/harvestscan/data/local/DiseaseInfo.kt
@@ -0,0 +1,164 @@
+package com.dicoding.harvestscan.data.local
+
+data class DiseaseInfo(
+    val description: String,
+    val tips: String
+)
+
+object DiseaseData {
+    val diseaseDetails = mapOf(
+        "Apple: Apple_scab" to DiseaseInfo(
+            description = "Description for Disease1",
+            tips = "Tips for Disease1"
+        ),
+        "Apple: Black_rot" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Apple: Cedar_apple_rust" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Apple: healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Blueberry: healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Cherry (including_sour): Powdery mildew" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Cherry (including_sour): healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Corn (maize): Cercospora leaf spot, Gray leaf spot" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Corn (maize): Common_rust_" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Corn (maize): Northern_Leaf_Blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Corn (maize): healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Grape: Black rot" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Grape: Esca (Black Measles)" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Grape: Leaf blight (Isariopsis Leaf Spot)" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Grape: healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Orange: Haunglongbing (Citrus greening)" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Peach: Bacterial spot" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Peach: healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Pepper, bell: Bacterial_spot" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Pepper, bell: healthy" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        ),
+        "Potato: Early blight" to DiseaseInfo(
+            description = "Description for Disease2",
+            tips = "Tips for Disease2"
+        )
+        // Tambahkan detail penyakit lainnya
+    )
+}
